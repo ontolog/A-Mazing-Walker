ab0cc718f44f1a8f325d11b8bc4e992b16031493
ontolog <kharkow@bigmir.net>
2015-02-20 23:00:27 +0200

    Revert "Revert "Update oracle.h""
    This reverts commit 477b4a5325fd5706446d18c31e91168b2038ffda.

--- LabWalker/oracle.h
+++ LabWalker/oracle.h
@@ -1,8 +1,58 @@
-#ifndef ORACLE_H
-#define ORACLE_H
+#pragma once
+#include <functional>
+#include <iostream>
+#include <map>
+#include <vector>
+#include <list>
+enum EDirections
+{
+	eNorth,
+	eSouth,
+	eWest,
+	eEast,
+	eTotal,
+	eNone
+};
+std::string FirstDirLetter[eTotal] = {"N","S","W", "E"};
+EDirections Opposite[eTotal] = { eSouth, eNorth, eEast, eWest };
 
+typedef std::map <std::string, double> mapSD;
 
+struct responce
+{
+	bool array[eTotal];
+	int movementToExit;
+};
+class TGameData
+{
+public:
+	void 
+	void setData()
+	{
 
+	}
+};
+class TOperator
+{
+public:
+	void setReceivedCallBack(std::function<void(responce)>)
+	{
 
-
-#endif
+	}
+};
+class Oracle
+{
+private:
+	mapSD			m_branch;
+	int				m_movementToexit;
+	bool			m_isUpdated;
+	TGameData*		m_gameData;
+	TOperator*		m_operator;
+public:
+	Oracle();
+	void PackageReceiveCallBack(const responce&);// bind 
+	int getData();
+	int analysisRating();
+	void doMovement();
+	bool isEfectiveMovement();
+};
